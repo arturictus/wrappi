48f6220605b35a386dff7dd1b0f97b65938f9f50
Artur Panach <arturictus@gmail.com>
2018-12-04 09:08:52 +0800

    refactor path_gen

--- lib/wrappi/path_gen.rb
+++ lib/wrappi/path_gen.rb
@@ -1,23 +1,23 @@
 module Wrappi
-  # TODO: REFACTOR!!
   class PathGen
-
+    class MissingParamError < StandardError; end
+    PATTERN = /:\w+/
     attr_reader :input_path, :input_params
     def initialize(input_path, input_params)
       @input_path = input_path
-      @input_params = input_params
-      @interpolable = input_path =~ /:\w+/
+      @input_params = Fusu::HashWithIndifferentAccess.new(input_params)
+      @interpolable = input_path =~ PATTERN
     end
 
     def compiled_path
       return input_path unless interpolable?
-      URI.escape(new_sections.join('/'))
+      @compiled_path ||= URI.escape(new_sections.join('/'))
     end
     alias_method :path, :compiled_path
 
     def processed_params
       return input_params unless interpolable?
-      input_params.reject{ |k, v| keys_in_params.include?(k) }
+      @processed_params ||= input_params.reject{ |k, v| keys_in_params.include?(k.to_sym) }
     end
     alias_method :params, :processed_params
 
@@ -28,26 +28,29 @@ module Wrappi
     end
 
     def new_sections
-      sections.map do |e|
-        if e =~ /:\w+/
-          i = e[1..-1]
-          input_params.fetch(i.to_sym)
+      sections.map do |section|
+        if section =~ PATTERN
+          key = section.delete(':')
+          raise MissingParamError.new("path: #{input_path} requires param ':#{key}'") unless input_params.key?(key)
+          input_params[key]
         else
-          e
+          section
         end
       end
     end
+
     def sections
       input_path.split("/")
     end
+
     def keys_in_params
       interpolations.map do |k|
-        i = k[1..-1]
-        i.to_sym
+        k.delete(':').to_sym
       end
     end
+
     def interpolations
-      input_path.scan(/:\w+/)
+      input_path.scan(PATTERN)
     end
   end
 end
--- spec/wrappi/path_gen_spec.rb
+++ spec/wrappi/path_gen_spec.rb
@@ -5,19 +5,24 @@ module Wrappi
       it 'adds param to the path and removes it from the params' do
         inst = described_class.new('/users/:user_id', user_id: 1, foo: 'baz')
         expect(inst.compiled_path).to eq '/users/1'
-        expect(inst.processed_params).to eq({ foo: 'baz' })
+        expect(inst.processed_params).to eq({"foo"=>"baz"})
       end
     end
     context 'without param in the path' do
       it 'does nothing' do
         inst = described_class.new('/users', user_id: 1, foo: 'baz')
         expect(inst.compiled_path).to eq '/users'
-        expect(inst.processed_params).to eq({ user_id: 1, foo: 'baz' })
+        expect(inst.processed_params).to eq({"foo"=>"baz", "user_id"=>1})
       end
     end
     context 'multiple params' do
-      it 'removes all params to the ' do
+      it 'removes all params to the input_params' do
         inst = described_class.new('/users/:user_id/comments/:foo', user_id: 1, foo: 'baz')
+        expect(inst.compiled_path).to eq '/users/1/comments/baz'
+        expect(inst.processed_params).to eq({})
+      end
+      it 'works with string keys' do
+        inst = described_class.new('/users/:user_id/comments/:foo', "user_id" => 1, "foo" => 'baz')
         expect(inst.compiled_path).to eq '/users/1/comments/baz'
         expect(inst.processed_params).to eq({})
       end
@@ -27,7 +32,7 @@ module Wrappi
         inst = described_class.new('/users/:user_id/comments/:foo', user_id: 1)
         expect {
           inst.compiled_path
-        }.to raise_error # TODO: specify error
+        }.to raise_error(PathGen::MissingParamError)
       end
     end
 
