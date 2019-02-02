ddfffbb6396226c8ebce44bb4102c3e05453abcc
Artur Panach <arturictus@gmail.com>
2019-02-02 15:11:41 +0800

    testing endpoint async

--- spec/wrappi/async_spec.rb
+++ spec/wrappi/async_spec.rb
@@ -14,15 +14,38 @@ module Wrappi
     end
   end
   describe Async do
-    context "existing endpoint" do
-      let(:options) { {} }
-      let(:async_options) { {foo: :bar} }
-      subject { described_class.perform_later(AsyncEndpoint.to_s, {params: {user_id: 1}, options: options}, async_options) }
-      it do
+    let(:options) { {} }
+    let(:async_options) { {foo: :bar} }
+    let(:params) { {user_id: 1} }
+    let(:endpoint_args) { {params: params, options: options} }
+
+    shared_examples "async flow" do
+      it "Job gets called with options" do
         subject
         expect(options.fetch(:success)).to be true
         expect(options.fetch(:async_options)).to eq async_options
       end
+    end
+
+    context "existing endpoint" do
+      subject { described_class.perform_later(AsyncEndpoint.to_s, endpoint_args, async_options) }
+      include_examples 'async flow'
+    end
+    context "When endpoint const does not exists" do
+      subject { described_class.perform_later("UnknowEndpoint", endpoint_args, async_options) }
+      it do
+        expect{ subject }.not_to raise_error 
+      end
+    end
+    describe "endpoint #async(opts = {})" do
+      before do
+        expect(described_class).to receive(:perform_later).with(AsyncEndpoint.to_s, endpoint_args, async_options).and_call_original
+      end
+      subject do
+        inst = AsyncEndpoint.new(params, options)
+        inst.async(async_options)
+      end
+      include_examples 'async flow'
     end
   end
 end
